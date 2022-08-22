9a0a4b791dbe22c6a2b3ce3422b7ddb26a0fa848
Md Minhaz Alam <111682171+mdminhaz0709@users.noreply.github.com>
2022-08-22 20:13:58 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,10 @@
+//5. WAP to calculate the length of String using printf function.
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=printf("ABCD");
+    printf("Length of string \"ABCD\" is %d",a);
+    return 0;   
+}
---
+++ 6.c
@@ -0,0 +1,9 @@
+/*6. WAP to print the name of the user in double quotes.
+Expected output format – “Hello , Md Minhaz”
+*/
+#include<stdio.h>
+int main()
+{
+    printf("\"Hello , Md Minhaz\"");
+    return 0;   
+}
